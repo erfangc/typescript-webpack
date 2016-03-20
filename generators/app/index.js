62bc3cb4d7092f3678ad5710312a231eb97b7385
Erfang Chen <erfangc@gmail.com>
2016-03-20 18:31:11 -0400

    major revamp

--- generators/app/index.js
+++ generators/app/index.js
@@ -3,21 +3,20 @@ var yeoman = require('yeoman-generator');
 var mkdirp = require('mkdirp');
 var chalk = require('chalk');
 var yosay = require('yosay');
+var _ = require('lodash');
 
 module.exports = yeoman.generators.Base.extend({
     prompting: function () {
         var done = this.async();
 
         // Have Yeoman greet the user.
-        this.log(yosay(
-            'Welcome to the bee\'s knees ' + chalk.red('typescript / webpack') + ' generator!'
-        ));
+        this.log(yosay(`Welcome to the ${chalk.red('typescript / webpack')} generator!`));
 
         var prompts = [{
             type: 'input',
             name: 'appname',
-            message: 'Your app\'s name',
-            default: this.appname
+            message: 'Enter a name for your App',
+            default: _.kebabCase(this.appname)
         }];
 
         this.prompt(prompts, function (props) {
@@ -45,12 +44,14 @@ module.exports = yeoman.generators.Base.extend({
             this.destinationPath('src/index.ts')
         );
         this.fs.copy(
-            this.templatePath('test/index.spec.ts'),
+            this.templatePath('test/jasmine/index.spec.ts'),
             this.destinationPath('test/index.spec.ts')
         );
     },
 
     install: function () {
+        var devDependencies = ["jasmine", "karma-jasmine", "karma", "karma-chrome-launcher", "karma-webpack", "ts-loader", "typescript", "typings", "webpack"];
+        this.npmInstall(devDependencies, {"saveDev": true});
         this.installDependencies();
         this.spawnCommand('typings', ['install']);
     }
